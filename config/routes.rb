03240b961517f339596d9baf94916fa267172c93
cardon <philantrope1@outlook.com>
2022-10-17 19:41:25 +0200

    essai stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root 'teddies#index'
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
